2641d9676f9caf28f277121fc3503cbff2b25ea6
Daniel Correa <yo@danielgara.com>
2024-03-29 12:04:43 -0500

    imp: C07-with-mysql

--- Chapter-07-mysql/moviesstore/moviesstore/settings.py
+++ Chapter-07-mysql/moviesstore/moviesstore/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'moviesstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moviesstore',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
