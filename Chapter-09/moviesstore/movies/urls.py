707bd3ddb0bbd9ad3add80f5fd6b42ef152718cb
Daniel Correa <yo@danielgara.com>
2024-04-11 10:53:09 -0500

    imp: C09 create and read reviews

--- Chapter-09/moviesstore/movies/urls.py
+++ Chapter-09/moviesstore/movies/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='movies.index'),
     path('<int:id>/', views.show, name='movies.show'),
+    path('<int:id>/create/', views.create_review, name='movies.create_review'),
 ]
